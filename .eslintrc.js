a91aaaf7e64dffabbe3c133c666a7645f80cd6f5
Michael Hadley <michaelwesthadley@gmail.com>
2017-05-14 10:24:56 -0500

    Brining over eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	// Enable core eslint rules, see: http://eslint.org/docs/rules/
+	extends: "eslint:recommended",
+	// Additional rules
+	rules: {
+		"brace-style": ["warn", "1tbs"],
+		"indent": ["warn", 4, {"SwitchCase": 1}],
+		"max-len": ["warn", 100, {"ignoreUrls": true}],
+		"no-mixed-spaces-and-tabs": "warn",
+		"no-console": ["warn", {"allow": ["warn", "error"]}]
+	},
+	env: {
+		browser: true,
+		es6: true
+	},
+	parserOptions: {
+		sourceType: "module"
+	}
+}
